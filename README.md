f0ebec1051056d3e58267a4731bbf91717c61640
t94j0 <augeocacher@gmail.com>
2017-08-01 15:09:34 -0400

    Updated to be better

--- README.md
+++ README.md
@@ -4,13 +4,14 @@
 1. Copy `reddit.py` file to recon-ng module folder
 ```
 cd /path/to/recon-ng/
-mkdir -p modules/discovery/info_disclosure
-cp /path/to/git/reddit.py modules/discovery/info_disclosure
+cp /path/to/git/reddit.py modules/recon/host-domains/
 ```
+
 2. Start recon-ng
 ```
 ./recon-ng
 ```
+
 3. `use reddit`
 4. `set SOURCE maxh.io`
 5. `run`
--- reddit.py
+++ reddit.py
@@ -1,4 +1,5 @@
 from recon.core.module import BaseModule
+from urllib.parse import urlparse
 import json
 
 class Module(BaseModule):
@@ -9,25 +10,31 @@ class Module(BaseModule):
       'query': 'SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL'
       }
   def module_run(self, hosts):
+    total_domains = 0
+
     for host in hosts:
+      self.verbose('Trying domain: {}'.format(host))
+      self.output("-------")
       url = 'http://reddit.com/domain/%s.json' % (host)
+
       try:
         resp = self.request(url)
         code = resp.status_code
+        if code == 200 or code == 301:
+          if resp.json == None:
+            self.alert("No data was found for the url")
+            return
+          parsed = json.loads(resp.text)
+          for child in parsed["data"]["children"]:
+            total_domains += 1
+            permalink = child['data']['permalink']
+            reddit_url = child['data']['url']
+            h = urlparse(reddit_url).hostname
+            self.add_domains(h)
+            self.alert('Permalink: http://reddit.com{}'.format(permalink))
+        else:
+          self.error('Error for domain {}'.format(url))
       except KeyboardInterrupt:
         raise KeyboardInterrupt
-      except:
-        code = 'Error'
-      if code == 200 or code == 301:
-        if resp.json == None:
-          self.alert("No data was found for the url")
-          return
-        parsed = json.loads(resp.text)
-        for child in parsed["data"]["children"]:
-          permalink = child['data']['permalink']
-          reddit_url = child['data']['url']
-          self.add_domains(reddit_url)
-          self.alert('Permalink: http://reddit.com%s' % (permalink))
-          self.alert('Added new domain')
-      else:
-        self.error('Error for domain %s' % (url))
+
+    self.alert('Added {} domains'.format(total_domains))
